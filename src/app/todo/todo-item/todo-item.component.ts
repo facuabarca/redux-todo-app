012dfe60c2b686c86d1352f6262be3e2c07bfac9
Facundo Abarca <inditex1@P20058.local>
2019-10-08 18:35:33 +0200

    se implementa visualizacion de listas y funcionalidad en todo-item

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Todo } from './../models/todo.model';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
+  @Input() todo: Todo;
+
+  checkField: FormControl;
+  txtInput: FormControl;
+  editando: boolean;
+
+  @ViewChild('txtInputFisico', null) txtFisico: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+    this.checkField = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+    console.log(this.todo);
+  }
+
+  editar() {
+    this.editando = true;
+    setTimeout(() => {
+      this.txtFisico.nativeElement.select();
+    }, (1));
+  }
+
+  terminarEdicion() {
+    this.editando = false;
   }
 
 }
--- src/app/todo/todo.reducer.ts
+++ src/app/todo/todo.reducer.ts
@@ -4,6 +4,8 @@ import * as fromTodo from './todo.actions';
 const todo1 = new Todo('Limpiar la habitación');
 const todo2 = new Todo('Salir a correr');
 
+todo2.completado = true;
+
 const estadoInicial: Todo[] = [todo1, todo2];
 
 export function todoReducer(state = estadoInicial, action: fromTodo.Acciones): Todo[] {
